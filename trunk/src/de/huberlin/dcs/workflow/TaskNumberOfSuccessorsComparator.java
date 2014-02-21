9abcef3d3aeefcccc71f00a2ba97e27b94db86a2
marc.bux@gmail.com <marc.bux@gmail.com@30b7e0ca-6d91-b342-e91b-9da06e61a7b3>
2014-02-21 15:08:03 +0000

    Changed package naming

---
+++ trunk/src/de/huberlin/dcs/workflow/TaskNumberOfSuccessorsComparator.java
@@ -0,0 +1,12 @@
+package de.huberlin.dcs.workflow;
+
+import java.util.Comparator;
+
+public class TaskNumberOfSuccessorsComparator implements Comparator<Task> {
+	
+	@Override
+	public int compare(Task task1, Task task2) {
+		return Double.compare(task2.getWorkflow().getGraph().getOutEdges(task2).size(), task1.getWorkflow().getGraph().getOutEdges(task1).size());
+	}
+
+}
---
+++ trunk/src/de/huberlin/dcs/workflow/TaskProgressRateComparator.java
@@ -0,0 +1,12 @@
+package de.huberlin.dcs.workflow;
+
+import java.util.Comparator;
+
+public class TaskProgressRateComparator implements Comparator<Task> {
+	
+	@Override
+	public int compare(Task task1, Task task2) {
+		return Double.compare(task1.getProgressRate(), task2.getProgressRate());
+	}
+
+}
